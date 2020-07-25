8305ec72d8663df7df6b3d6778dd52c05adfbcd1
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-07-25 12:57:16 +0900

    TBD

--- 4x4x4solver_v3/4x4x4solver.cpp
+++ 4x4x4solver_v3/4x4x4solver.cpp
@@ -5,21 +5,92 @@
 #include <chrono>
 #include <utility>
 #include <string>
-#include <cmath>
+//#include <cmath>
 #include <fstream>
 #include <sstream>
+#include <array>
 
 #define rep(i, n, m) for(int i = (n); (i) < (m); (i)++)
 #define len(arr) sizeof(arr) / sizeof(*arr)
 
 using namespace std;
 
+const array<int, 8> cp_d = {0, 1, 2, 3, 4, 5, 6, 7};
+const array<int, 8> co_d = {0, 0, 0, 0, 0, 0, 0, 0};
+const array<int, 24> ep_d = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+const array<int, 24> ce_d = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5};
+
+class Cube{
+    public:
+    array<int, 8> Cp;
+    array<int, 8> Co;
+    array<int, 24> Ep;
+    array<int, 24> Ce;
+    void set(array<int, 8> cp, array<int, 8> co, array<int, 24> ep, array<int, 24> ce);
+    array<int, 8> move_cp(int twist);
+    array<int, 8> move_co(int twist);
+    array<int, 24> move_ep(int twist);
+    array<int, 24> move_ce(int twist);
+};
+
+void Cube::set(array<int, 8> cp, array<int, 8> co, array<int, 24> ep, array<int, 24> ce){
+    rep(i, 0, 8) this->Cp[i] = cp[i];
+    rep(i, 0, 8) this->Co[i] = co[i];
+    rep(i, 0, 24) this->Ep[i] = ep[i];
+    rep(i, 0, 24) this->Ce[i] = ce[i];
+}
+
+array<int, 8> Cube::move_cp(int twist){
+    int surface[6][4] = {{3, 1, 7, 5}, {0, 2, 4, 6}, {0, 1, 3, 2}, {4, 5, 7, 6}, {2, 3, 5, 4}, {1, 0, 6, 7}};
+    array<int, 8> res;
+    rep(i, 0, 8) res[i] = this->Cp[i];
+    int type = twist / 6;
+    int amount = twist % 3;
+    rep(i, 0, 4) res[surface[type][(i + amount + 1) % 4]] = this->Cp[surface[type][i]];
+    return res;
+}
+
+array<int, 8> Cube::move_co(int twist){
+    int surface[6][4] = {{3, 1, 7, 5}, {0, 2, 4, 6}, {0, 1, 3, 2}, {4, 5, 7, 6}, {2, 3, 5, 4}, {1, 0, 6, 7}};
+    int pls[4] = {2, 1, 2, 1}
+    array<int, 8> res;
+    rep(i, 0, 8) res[i] = this->Cp[i];
+    int type = twist / 6;
+    int amount = twist % 3;
+    rep(i, 0, 4){
+        res[surface[type][(i + amount + 1) % 4]] = this->Cp[surface[type][i]];
+        if(int(type / 2) != 1 && amount != 1){
+            res[surface[type][(i + amount + 1) % 4]] += pls[(i + amount + 1) % 4];
+            res[surface[type][(i + amount + 1) % 4]] %= 3;
+        }
+    }
+    return res;
+}
+
+array<int, 24> Cube::move_ep(int twist){
+    int surface[6][4] = {{3, 1, 7, 5}, {0, 2, 4, 6}, {0, 1, 3, 2}, {4, 5, 7, 6}, {2, 3, 5, 4}, {1, 0, 6, 7}};
+    int pls[4] = {2, 1, 2, 1}
+    array<int, 8> res;
+    rep(i, 0, 8) res[i] = this->Cp[i];
+    int type = twist / 6;
+    int amount = twist % 3;
+    rep(i, 0, 4){
+        res[surface[type][(i + amount + 1) % 4]] = this->Cp[surface[type][i]];
+        if(int(type / 2) != 1 && amount != 1){
+            res[surface[type][(i + amount + 1) % 4]] += pls[(i + amount + 1) % 4];
+            res[surface[type][(i + amount + 1) % 4]] %= 3;
+        }
+    }
+    return res;
+}
+
+
 
 //                            0    1     2     3     4      5      6    7     8     9     10     11     12   13    14    15    16     17     18   19   20     21    22     23     24   25    26    27    28     29     30   31    32    33    34     35
-string move_candidate[36] = {"R", "R2", "R'", "Rw", "Rw2", "Rw'", "L", "L2", "L'", "Lw", "Lw2", "Lw'", "U", "U2", "U'", "Uw", "Uw2", "Uw'", "D", "D2", "D'", "Dw", "Dw2", "Dw'", "F", "F2", "F'", "Fw", "Fw2", "Fw'", "B", "B2", "B'", "Bw", "Bw2", "Bw'"};
-int twist_to_idx[36] = {0, 1, 2, 3, 4, 5, 6, 7, 8, -1, -1, -1, 9, 10, 11, 12, 13, 14, 15, 16, 17, -1, -1, -1, 18, 19, 20, 21, 22, 23, 24, 25, 26, -1, -1, -1};
+const string move_candidate[36] = {"R", "R2", "R'", "Rw", "Rw2", "Rw'", "L", "L2", "L'", "Lw", "Lw2", "Lw'", "U", "U2", "U'", "Uw", "Uw2", "Uw'", "D", "D2", "D'", "Dw", "Dw2", "Dw'", "F", "F2", "F'", "Fw", "Fw2", "Fw'", "B", "B2", "B'", "Bw", "Bw2", "Bw'"};
+const int twist_to_idx[36] = {0, 1, 2, 3, 4, 5, 6, 7, 8, -1, -1, -1, 9, 10, 11, 12, 13, 14, 15, 16, 17, -1, -1, -1, 18, 19, 20, 21, 22, 23, 24, 25, 26, -1, -1, -1};
 
-vector<vector<int>> successor = 
+const vector<vector<int>> successor = 
     {
     vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8,            12, 13, 14, 15, 16, 17, 18, 19, 20,             24, 25, 26, 27, 28, 29, 30, 31, 32            }, // phase 0
     vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8,            12, 13, 14,     16,     18, 19, 20,             24, 25, 26,     28,     30, 31, 32            }, // phase 1
@@ -84,7 +155,18 @@ void get_prunning(){
 }
 
 int main() {
-    get_move_ce_phase0();
-    get_prunning();
+    //get_move_ce_phase0();
+    //get_prunning();
+    Cube cube;
+    cube.set(cp_d, co_d, ep_d, ce_d);
+    rep(i, 0, 8){
+        cout << cube.Cp[i] << ' ';
+    }
+    cout << endl;
+    cube.set(cube.move_cp(0), cube.move_co(0), ep_d, ce_d);
+    rep(i, 0, 8){
+        cout << cube.Cp[i] << ' ';
+    }
+    cout << endl;
     return 0;
 }
