4090b7d23bc4a5cf9a1d6ae4b21f938ad64ad3fc
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-08-14 10:58:42 +0900

    solvour

--- Solvour1/Solvour1_arduino/Solvour1_arduino.ino
+++ Solvour1/Solvour1_arduino/Solvour1_arduino.ino
@@ -5,10 +5,7 @@ const long turn_steps = 400;
 const int step_dir[2] = {11, 9};
 const int step_pul[2] = {12, 10};
 const int sensor[2] = {14, 15};
-//const int grab_deg[2] = {79, 75};
-//const int release_deg[2] = {101, 97};
-const int grab_deg[2] = {74, 74};
-const int release_deg[2] = {96, 96};
+const int deg[2][4] = {{50, 60, 70, 80}, {50, 60, 70, 80}}
 const int offset = 3;
 
 char buf[30];
@@ -33,54 +30,35 @@ void move_motor(long num, long deg, long spd) {
   for (int i = 0; i < accel; i++) {
     motor_hl = !motor_hl;
     digitalWrite(step_pul[num], motor_hl);
+    /*
     if (analogRead(sensor[num1]) > magnet_threshold)
       digitalWrite(step_pul[num1], motor_hl);
+    */
     delayMicroseconds(max_time - slope * i);
   }
   for (int i = 0; i < steps * 2 - accel * 2; i++) {
     motor_hl = !motor_hl;
     digitalWrite(step_pul[num], motor_hl);
+    /*
     if (analogRead(sensor[num1]) > magnet_threshold)
       digitalWrite(step_pul[num1], motor_hl);
+    */
     delayMicroseconds(avg_time);
   }
   for (int i = 0; i < accel; i++) {
     motor_hl = !motor_hl;
     digitalWrite(step_pul[num], motor_hl);
+    /*
     if (analogRead(sensor[num1]) > magnet_threshold)
       digitalWrite(step_pul[num1], motor_hl);
+    */
     delayMicroseconds(max_time - slope * accel + accel * (i + 1));
   }
-  /*
-  while (analogRead(sensor[num]) > magnet_threshold) {
-    motor_hl = !motor_hl;
-    digitalWrite(step_pul[num], motor_hl);
-    if (analogRead(sensor[num1]) > magnet_threshold)
-      digitalWrite(step_pul[num1], motor_hl);
-    if (Serial.available()) break;
-    delayMicroseconds(max_time);
-  }
-  Serial.println('y');
-  */
 }
 
-
-void release_arm(int num) {
-  if (num == 0)servo0.write(release_deg[num] + offset);
-  else servo1.write(release_deg[num] + offset);
-  /*
-    delay(70);
-    if (num == 0)servo0.write(release_deg[num]);
-    else servo1.write(release_deg[num]);
-  */
-}
-
-void grab_arm(int num) {
-  if (num == 0)servo0.write(grab_deg[num] - offset);
-  else servo1.write(grab_deg[num] - offset);
-  delay(70);
-  if (num == 0)servo0.write(grab_deg[num]);
-  else servo1.write(grab_deg[num]);
+void move_arm(int arm, int arg){
+  if (arm == 0) servo0.write(deg[arm][arg]);
+  else servo1.write(deg[arm][arg]);
 }
 
 void setup() {
@@ -109,8 +87,7 @@ void loop() {
       data[0] = atoi(strtok(buf, " "));
       data[1] = atoi(strtok(NULL, " "));
       data[2] = atoi(strtok(NULL, " "));
-      if (data[1] == 1000) grab_arm(data[0]);
-      else if (data[1] == 2000) release_arm(data[0]);
+      if (data[1] >= 1000) move_arm(data[0], data[1] / 1000 - 1);
       else move_motor(data[0], data[1], data[2]);
       idx = 0;
     }
