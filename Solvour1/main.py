b4b1cf8a9028d9bacd6e3dcd0bc6aa67d7721eff
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-08-18 22:40:43 +0900

    detect

--- Solvour1/main.py
+++ Solvour1/main.py
@@ -45,7 +45,7 @@ def inspection_p():
             4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
             ]
     '''
-    #state = detect()
+    state = detect()
     fill_box(state)
     #solution = solver(state, [0.5, 5, 2, 2, 2, 3], 30)
     solution = [0, 12, 2, 14]
@@ -55,46 +55,53 @@ def inspection_p():
 # Get colors of stickers
 def detect():
     state = [-1 for _ in range(96)]
-    capture = cv2.VideoCapture(2)
+    capture = cv2.VideoCapture(0)
     for face in range(6):
-        #color: g, b, r, o, y, w
-        # for normal sticker
-        color_low = [[50, 50, 50],   [90, 50, 50],   [160, 140, 50], [160, 50, 50],  [20, 20, 50],   [0, 0, 50]]
-        color_hgh = [[90, 255, 255], [140, 255, 255], [20, 255, 255], [20, 140, 255], [50, 255, 255], [179, 40, 255]]
-        #color_low = [[40, 50, 50],   [90, 50, 50],   [160, 70, 50], [0, 20, 50],  [20, 50, 50],   [0, 0, 50]]
-        #color_hgh = [[90, 255, 255], [140, 255, 255], [180, 255, 200], [20, 255, 255], [40, 255, 255], [179, 50, 255]]
-        color_idx = [1, 3, 2, 4, 5, 0]
-        circlecolor = [(0, 255, 0), (255, 0, 0), (0, 0, 255), (0, 170, 255), (0, 255, 255), (255, 255, 255)]
-        d = 25
-        size_x = 400
-        size_y = 300
-        center = [size_x // 2, size_y // 2]
-        delta = [-3, -1, 1, 3]
-        loopflag = [1 for _ in range(16)]
-        while sum(loopflag):
-            ret, frame = capture.read()
-            frame = cv2.robot_solutionize(frame, (size_x, size_y))
-            hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-            for y in range(4):
-                for x in range(4):
-                    y_coord = center[1] + d * delta[y]
-                    x_coord = center[0] + d * delta[x]
-                    idx = face * 16 + y * 4 + x
-                    cv2.circle(frame, (x_coord, y_coord), 5, (0, 0, 0), thickness=3, lineType=cv2.LINE_8, shift=0)
-                    val = hsv[y_coord, x_coord]
-                    for color in range(6):
-                        flag = True
-                        for k in range(3):
-                            if not ((color_low[color][k] < color_hgh[color][k] and color_low[color][k] <= val[k] <= color_hgh[color][k]) or (color_low[color][k] > color_hgh[color][k] and (color_low[color][k] <= val[k] or val[k] <= color_hgh[color][k]))):
-                                break
-                        else:
-                            cv2.circle(frame, (x_coord, y_coord), 15, circlecolor[color], thickness=3, lineType=cv2.LINE_8, shift=0)
-                            cv2.circle(frame, (x_coord, y_coord), 20, (0, 0, 0), thickness=2, lineType=cv2.LINE_8, shift=0)
-                            if cv2.waitKey() == 32:
-                                state[idx] = color_idx[color]
-                                loopflag[y * 4 + x] = 0
-                                break
-            cv2.imshow('frame', frame)
+        for mode in range(2):
+            #color: g, b, r, o, y, w
+            # for normal sticker
+            color_low = [[50, 50, 50],   [90, 50, 50],   [160, 140, 50], [160, 50, 50],  [20, 20, 50],   [0, 0, 50]]
+            color_hgh = [[90, 255, 255], [140, 255, 255], [20, 255, 255], [20, 140, 255], [50, 255, 255], [179, 40, 255]]
+            #color_low = [[40, 50, 50],   [90, 50, 50],   [160, 70, 50], [0, 20, 50],  [20, 50, 50],   [0, 0, 50]]
+            #color_hgh = [[90, 255, 255], [140, 255, 255], [180, 255, 200], [20, 255, 255], [40, 255, 255], [179, 50, 255]]
+            color_idx = [1, 3, 2, 4, 5, 0]
+            circlecolor = [(0, 255, 0), (255, 0, 0), (0, 0, 255), (0, 170, 255), (0, 255, 255), (255, 255, 255)]
+            d = 25
+            size_x = 400
+            size_y = 300
+            center = [size_x // 2, size_y // 2]
+            delta = [-3, -1, 1, 3]
+            loopflag = [1 for _ in range(16)]
+            mode_flag = [{0, 4, 5, 6, 7, 8, 9, 10, 11, 15}, {1, 2, 3, 12, 13, 14}]
+            while sum([loopflag[i] for i in mode_flag[mode]]):
+                ret, frame = capture.read()
+                frame = cv2.resize(frame, (size_x, size_y))
+                hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+                for y in range(4):
+                    for x in range(4):
+                        if not y * 4 + x in mode_flag[mode]:
+                            continue
+                        y_coord = center[1] + d * delta[y]
+                        x_coord = center[0] + d * delta[x]
+                        idx = face * 16 + y * 4 + x
+                        cv2.circle(frame, (x_coord, y_coord), 5, (0, 0, 0), thickness=3, lineType=cv2.LINE_8, shift=0)
+                        val = hsv[y_coord, x_coord]
+                        for color in range(6):
+                            flag = True
+                            for k in range(3):
+                                if not ((color_low[color][k] < color_hgh[color][k] and color_low[color][k] <= val[k] <= color_hgh[color][k]) or (color_low[color][k] > color_hgh[color][k] and (color_low[color][k] <= val[k] or val[k] <= color_hgh[color][k]))):
+                                    break
+                            else:
+                                cv2.circle(frame, (x_coord, y_coord), 15, circlecolor[color], thickness=3, lineType=cv2.LINE_8, shift=0)
+                                cv2.circle(frame, (x_coord, y_coord), 20, (0, 0, 0), thickness=2, lineType=cv2.LINE_8, shift=0)
+                                if cv2.waitKey() == 32:
+                                    state[idx] = color_idx[color]
+                                    loopflag[y * 4 + x] = 0
+                                    break
+                cv2.imshow('frame', frame)
+            print(face, mode, 'done')
+            while cv2.waitKey() != ord('n'):
+                continue
         print(face, 'done')
     capture.release()
     return state
