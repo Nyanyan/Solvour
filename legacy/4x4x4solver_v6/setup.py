f8416a7c851d9540f39469a443aeee2ecafb0466
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-12-14 18:10:21 +0900

    start to write solver v8

--- legacy/4x4x4solver_v6/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-from numpy import get_include # cimport numpy を使うため
-
-ext = Extension("cube_class_c_6", sources=["cube_class_c.pyx"], include_dirs=['.', get_include()])
-setup(name="cube_class_c_6", ext_modules=cythonize([ext]))
